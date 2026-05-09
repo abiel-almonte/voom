7d63d30df85432b4efa757571cd2f2de0c1de45e
Abiel Almonte <almonteabiel@gmail.com>
2026-05-09 16:39:27 -0400

    Add arg parse to select model ckpt

--- scripts/convert_to_safetensors.py
+++ scripts/convert_to_safetensors.py
@@ -1,6 +1,18 @@
+import argparse
 import torch
 from safetensors.torch import save_file
+from pathlib import Path
 
-ckpt = torch.load("voom.pt", weights_only=True)
-state = ckpt["model"] if "model" in ckpt else ckpt
-save_file(state, "voom.safetensors")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ckpt", type=str, required=True)
+    
+    args = parser.parse_args()
+    ckpt_path = Path(args.ckpt)
+
+    ckpt = torch.load(ckpt_path, weights_only=True)
+    state = ckpt["model"] if "model" in ckpt else ckpt
+    save_file(state, "voom.safetensors")
+
+if __name__ == "__main__":
+    main()
